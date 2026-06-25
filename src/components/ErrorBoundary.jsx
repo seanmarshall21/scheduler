334686c9af168f5f919a33cc75cc05e45f7f2016
seanmarshall21 <sean@crssd.com>
2026-06-25 04:41:28 -0700

    Drop unused eslint-disable in ErrorBoundary

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -13,7 +13,6 @@ export default class ErrorBoundary extends Component {
   }
 
   componentDidCatch(error, info) {
-    // eslint-disable-next-line no-console
     console.error('[Commons] crashed:', error, info?.componentStack);
   }
 
